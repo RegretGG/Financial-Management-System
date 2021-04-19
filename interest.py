bb35a7b8c7bf524301010d189a80007a2e70f91a
RegretGG <69192923+RegretGG@users.noreply.github.com>
2021-04-19 08:58:39 +0400

    Update interest.py

--- interest.py
+++ interest.py
@@ -2,6 +2,6 @@ import time
 import methods
 
 while True:
-  methods.comp_interest() /increments interest every 30 seconds
+  methods.comp_interest() #increments interest every 30 seconds
   time.sleep(30)
     
