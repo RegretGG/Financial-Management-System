62385ed42217c1d8ac9bb41557da1b58d1f485ee
RegretGG <69192923+RegretGG@users.noreply.github.com>
2021-04-19 09:02:40 +0400

    Update methods.py

--- methods.py
+++ methods.py
@@ -5,10 +5,10 @@ from email.mime.text import MIMEText
 import mysql.connector as my
 
 try:
-  mydb=my.connect(host='localhost',user='root',database='python_bank',password="gautham2004")
+  mydb=my.connect(host='localhost',user='root',database='python_bank',password="password")
   C=mydb.cursor()
 except:
-  mydb=my.connect(host='localhost',user='root',password="gautham2004")
+  mydb=my.connect(host='localhost',user='root',password="password")
   C=mydb.cursor()
   C.execute("create database python_bank")
   C.execute("create table python_bank.account(acc_name varchar(25),balance integer(100),password varchar(25),email varchar(25))")
